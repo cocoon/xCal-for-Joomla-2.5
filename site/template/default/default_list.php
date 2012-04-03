c14483f3771fb1378877319b74ef8760b313d9e5
cocoon <nemea@gmx.net>
2012-04-03 16:42:07 +0300

    Fix closing div back for template/default/default_list.php

--- site/template/default/default_list.php
+++ site/template/default/default_list.php
@@ -1,1 +1,56 @@
-<div id="xcal">	<?php  	if($stamp->container->navigator): ?>		<?php echo $stamp->container->navigator; ?>	<div class="clr"></div>	<?php endif ?>		<div class="items">	<?php     if ($stamp->data->items)  {    $stampitems = $stamp->data->items;  }  else  {    $stampitems = $stamp->items;  }     if($stampitems){      foreach ($stampitems as $item){ ?>    			<div class="event">    				<?php if ($item->next): ?>  					<div class="box_date" style="background:<?php echo $item->cat_color; ?>">  							<?php echo $item->day.'<br/>'.$item->monthShort; ?>  					</div>  				<?php endif; ?>  				  				<div class="xcalcontent">  				  					<div>  						<h2>  							<a href="<?php echo $item->url; ?>" alt="<?php echo $item->title; ?>"><?php echo $item->title; ?></a> [<?php echo $item->cat_title; ?>]  						</h2>  						<div><?php if ($item->share) echo $item->share; ?></div>  						<?php echo $item->descShort; ?>  						<div class="clr"></div>  					</div>  					  					<?php if ($item->gcalendar): ?>  						<a class="gcal" href="<?php echo $item->gcalendarUrl; ?>" alt="aggiungi a google calendar">Google Calendar</a>  					<?php endif; ?>	  					  				</div>  				  				<div class="clr"></div>    			</div>        	<?php }  } ?>	</div>	<!-- </div> -->      
+<div id="xcal">
+	<?php  	if($stamp->container->navigator): ?>
+		<?php echo $stamp->container->navigator; ?>
+	<div class="clr"></div>
+	<?php endif ?>
+	
+	<div class="items">
+	<?php 
+  
+  if ($stamp->data->items)
+  {
+    $stampitems = $stamp->data->items;
+  }
+  else
+  {
+    $stampitems = $stamp->items;
+  } 
+  
+  if($stampitems){
+  
+    foreach ($stampitems as $item){ ?>
+  
+  			<div class="event">
+  
+  				<?php if ($item->next): ?>
+  					<div class="box_date" style="background:<?php echo $item->cat_color; ?>">
+  							<?php echo $item->day.'<br/>'.$item->monthShort; ?>
+  					</div>
+  				<?php endif; ?>
+  				
+  				<div class="xcalcontent">
+  				
+  					<div>
+  						<h2>
+  							<a href="<?php echo $item->url; ?>" alt="<?php echo $item->title; ?>"><?php echo $item->title; ?></a> [<?php echo $item->cat_title; ?>]
+  						</h2>
+  						<div><?php if ($item->share) echo $item->share; ?></div>
+  						<?php echo $item->descShort; ?>
+  						<div class="clr"></div>
+  					</div>
+  					
+  					<?php if ($item->gcalendar): ?>
+  						<a class="gcal" href="<?php echo $item->gcalendarUrl; ?>" alt="aggiungi a google calendar">Google Calendar</a>
+  					<?php endif; ?>	
+  					
+  				</div>
+  				
+  				<div class="clr"></div>
+  
+  			</div>
+        
+	<?php }
+  } ?>
+	</div>
+	
+</div>
